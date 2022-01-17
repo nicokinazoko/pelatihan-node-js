77ced75caedea5452bee28a9d4c70708ca8b3150
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2022-01-17 14:55:30 +0700

    Update app.js

--- Pertemuan-1/core-modules/app.js
+++ Pertemuan-1/core-modules/app.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const {
+    type
+} = require('os');
 
 
 // create file
@@ -8,7 +11,51 @@ const fs = require('fs');
 
 
 // read file
-fs.readFile('./mhs.txt', "utf-8", (err, data) => {
-    if(err) throw err;
-    consol,.
+// fs.readFile('./mhs.txt', "utf-8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+
+// // insert data json
+
+let dataJson = [{
+    "nama": "Nico",
+    "umur": 15
+}];
+
+let dataJsonInput = JSON.stringify(dataJson);
+// let dataJsonInput =
+
+fs.writeFile("./dataMahasiswa.json", dataJsonInput, (err) => {
+    console.log(err);
 })
+
+let pushData = [{
+    "nama": "Nico Satu",
+    "umur": 16
+}];
+
+// read json 
+fs.readFile('./dataMahasiswa.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    const dataDiri = JSON.parse(data);
+    // console.log(dataDiri);
+    // console.log(dataDiri);
+    dataDiri.push(pushData);
+    simpanData(dataDiri);
+    // console.log(dataDiri);
+})
+
+// let dataJsonInputBaru = JSON.stringify(pushData);
+
+// fs.writeFile('./dataMahasiswa.json', dataJsonInputBaru, (err) => {
+//     console.log(err);
+// })
+
+let simpanData = (dataInput) => {
+    let dataJsonInputBaru = JSON.stringify(dataInput);
+    fs.writeFile('./dataMahasiswa.json', dataJsonInputBaru, (err) => {
+        console.log(err);
+    })
+    // console.log(dataJsonInputBaru);
+}
